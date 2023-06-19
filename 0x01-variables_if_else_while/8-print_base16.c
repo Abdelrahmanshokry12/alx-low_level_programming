8ad72ae3c870d4c309e10e0feb2e8bd3dfb71093
Abdelrahmanshokry12 <king.2002.az@gmail.com>
2023-06-19 15:56:22 -0700

    ,l,

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,11 +12,12 @@ int main(void)
 {
 	int digit = 48;
 
-	while (digit < 102)
+	while (digit <= 102)
 	{
-	putchar(digit);
-	if (digit == 57)
-		digit += 39;
+		putchar(digit);
+
+		if (digit == 57)
+			digit += 39;
 		digit++;
 	}
 
