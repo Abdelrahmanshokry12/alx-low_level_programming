4a4ec822f9d79e785b9152f7b1783455d86d8269
Abdelrahmanshokry12 <king.2002.az@gmail.com>
2023-06-20 16:58:23 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * Description: print _putchar
+ * print _putchar
 */
 
 void print_alphabet(void)
 {
 	int ch;
 
-	for (ch = 'a'; ch < 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
-
-	return (0);
 }
