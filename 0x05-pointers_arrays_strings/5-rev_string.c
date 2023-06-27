d2b5efb6a74c75e6325c52b1f5b69c178812fff5
Abdelrahmanshokry12 <king.2002.az@gmail.com>
2023-06-27 12:09:26 -0700

    mj

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,13 +12,14 @@ void rev_string(char *s)
 	int len, i;
 	char temp;
 
+	/*find string length without null char*/
 	for (len = 0; s[len] != '\0'; ++len)
-	{
+		;
+
 		for (i = 0; i < len / 2; i++)
 		{
 			temp = s[i];
 			s[i] = s[len - 1 - i];
 			s[len - 1 - i] = temp;
 		}
-	}
 }
