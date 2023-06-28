08af52a0de8395c9f748fa5e384ba44ec8038599
Abdelrahmanshokry12 <king.2002.az@gmail.com>
2023-06-27 19:28:11 -0700

    qwds

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,30 +9,22 @@
 
 int _atoi(char *s)
 {
-	unsigned int num = 0;
+	int num = 0;
 	int sign = 1;
 
-	if (*s == '-')
-	{
-		sign *= -1;
-		s++;
-	}
-	else if (*s >= '0' && *s <= '9')
-	{
-		num = (*s - '0');
-		s++;
-	}
 	while (*s)
 	{
+		if (*s == '-')
+		sign *= -1;
+
 		if (*s >= '0' && *s <= '9')
 		{
-			num = (num * 10) + (*s - '0');
-		}
-		else
-		{
+			num = num * 10 + (*s - '0');
+			if (*(s + 1) < '0' || *(s + 1) > '9')
 			break;
 		}
-	s++;
+
+		s++;
 	}
 
 	return (num * sign);
