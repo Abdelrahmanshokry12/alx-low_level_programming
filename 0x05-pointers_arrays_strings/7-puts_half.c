63eae5c7cb24d63f310b2ede753fa0ad2e93512b
Abdelrahmanshokry12 <king.2002.az@gmail.com>
2023-06-27 18:25:34 -0700

    dfg

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,8 +13,12 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; ++i)
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
+		;
+	}
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
