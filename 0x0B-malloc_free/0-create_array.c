b2bb7a71863fa530daa7b62eb6a9ff755998b3f4
Abdelrahmanshokry12 <king.2002.az@gmail.com>
2023-07-12 10:59:23 -0700

    a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,15 +9,20 @@
 char *create_array(unsigned int size, char c)
 {
 	if (size == 0)
-	return (NULL);
+	{
+		return (NULL);
+	}
 
 	char *array = malloc(size * sizeof(char));
 
 	if (array == NULL)
+	{
 		return (NULL);
+	}
 
 	while (size--)
+	{
 		array[size] = c;
-
+	}
 	return (array);
 }
