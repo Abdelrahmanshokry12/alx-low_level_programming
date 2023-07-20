4f52331cce803a9cbdbceb15e2bf4ccda6f52ae8
Abdelrahmanshokry12 <king.2002.az@gmail.com>
2023-07-19 17:15:45 -0700

    wq

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: the number of command-line arguments
+ * @argv: an array of strings containing the command-line arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	char *p = (char *)main;
+	int b;
+
+	if (argc != 2)
+	{
+		printf("Error: Usage: %s <number of bytes to print>\n", argv[0]);
+		exit(1);
+	}
+
+	b = atoi(argv[1]);
+
+	if (b < 0)
+	{
+		printf("Error: Number of bytes must be positive\n");
+		exit(2);
+	}
+
+	while (b--)
+	{
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+	}
+
+	return (0);
+}
