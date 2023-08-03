0d2d6ea2fcca786c6c811a6e7580544cd832c703
Abdelrahmanshokry12 <king.2002.az@gmail.com>
2023-08-03 13:28:14 -0700

    q

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness - ll
+ * @void: aa
+ * Return: 1 or 0.
+*/
+
+int get_endianness(void)
+{
+	unsigned int num = 1;
+
+	char *ptr = (char *)&num;
+	if (*ptr)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
