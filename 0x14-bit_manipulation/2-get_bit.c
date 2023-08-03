44ac79ac36f7d12b6cbd85a16f7158818ec0d9d9
Abdelrahmanshokry12 <king.2002.az@gmail.com>
2023-08-03 13:18:39 -0700

    s

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,12 +10,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int mask;
+
 	if (index >= sizeof(n) * 8)
 	{
 		return (-1);
 	}
-	unsigned long int mask;
-
 	mask = 1UL << index;
 	return ((n & mask) ? 1 : 0);
 }
